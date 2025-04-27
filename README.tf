729dfc7aae2a0c209599de23d4f2c14d53918ce3
Martin Tamashiro <martintama@gmail.com>
2025-04-27 09:04:07 +0200

    feat: improve logging

--- README.tf
+++ README.tf
@@ -4,19 +4,46 @@
 
 ### App
 
+The application code is located in the `app` directory.
+
+This is a standard Go application following conventional Go project structure.
+
+A Makefile is provided with common operations:
+
+`make test`: Run tests
+`make build`: Build app
+
+View all available commands with with `make help`.
+
+#### Running the app
+
+First, build the application from the `app` directory:
+
+`make build`
+
+Then run it with:
+
+`./bin/domain-checker check --domain domaintocheck.com` 
+
 ### Terraform code
 
-This expects a local AWS SSO session named `tf` is configured. You can do so by running:
+Terraform code is located in the `tf` directory.
+
+#### Prerequisites
+
+Terraform configuration expects a local AWS SSO session named `tf-admin` is configured. Set this up with:
 
 `aws configure sso`
 
-And follow the instructions. When asked about the **Session name**, enter `tf` (Recommended). If you want to use a different name,
-please ensure scripts and files are changed to reflect the new naming.
+Follow the instructions. When asked about the **Session name**, enter `tf-admin` (Recommended). If you want to use a different name, please ensure scripts and files are changed accordingly.
 
-With that out of the way:
+>![IMPORTANT]
+> Terraform code needs to create new IAM roles for Lambda functions. The AWS `PowerUserAccess` role is insufficient - you'll need administrative privileges.
 
-1. Run `make login`
-2. Run `source ./set-aws.sh`. This will set the AWS creds as environment variables so they can be used by the s3 backend for the state.
-3. (Change whatever needs to be changed)
-4. `make plan`. This will trigger the `terraform plan` command.
-5. `make apply`. This will run the `terraform apply`
+#### Deployment
+
+1. Run `make login` to authenticate with SSO
+1. Run `source ./set-aws.sh` to configure AWS credentials as environment variables for the S3 backend state.
+1. (First time only) `make init`. This calls `terraform init` on the directory.
+1. `make plan`. This will trigger the `terraform plan` command.
+1. `make apply`. This will run the `terraform apply`
