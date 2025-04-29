e4e0af8b280e6906db6e9a3a7f086f4252598e70
Martin Tamashiro <martintama@gmail.com>
2025-04-29 06:54:21 +0200

    chore: improve README

--- README.tf
+++ README.tf
@@ -1,5 +1,19 @@
 # Domain checker
 
+This is a super basic application that runs on AWS Lambda and checks the availability status of a given domain name. It sends an email notification when (or IF) it gets released.
+
+Checks are made using a simple whois lookup, trying to stay under the radar, as any other action like opening the URL in the browser could signal there's interest to the owners.
+
+## Components
+
+This is composed of two parts:
+- App: Written in go, can be run locally to confirm the domain in question is effectively detected as not available to start with.
+- TF code: Builds and deploys the app to an AWS Lambda function, that is configured to run hourly and log the results to Clodwatch Logs. From there, a Cloudwatch Metric Filter looks for the text "DomainAvailable". If found, it sends an email to the pre-configured email.
+
+Currently it sends an email every time it runs (i.e. hourly) if the domain is found available. It would have been better to have a "cooldown period", and only send emails every 2 days, for example. Unfortunately, it would have meant adding more innecessary components to the mix, increasing the overall solution complexity. 
+
+On the other hand, worst case scenario is that we get 8-10 email while we sleep, but we won't miss the news!
+
 ## How to run this locally?
 
 ### App
@@ -13,7 +27,7 @@ A Makefile is provided with common operations:
 `make test`: Run tests
 `make build`: Build app
 
-View all available commands with with `make help`.
+View all available commands with `make help`.
 
 #### Running the app
 
@@ -38,7 +52,7 @@ Terraform configuration expects a local AWS SSO session named `tf-admin` is conf
 Follow the instructions. When asked about the **Session name**, enter `tf-admin` (Recommended). If you want to use a different name, please ensure scripts and files are changed accordingly.
 
 >![IMPORTANT]
-> Terraform code needs to create new IAM roles for Lambda functions. The AWS `PowerUserAccess` role is insufficient - you'll need administrative privileges.
+> Terraform code needs to create new IAM roles for Lambda functions. Thus, AWS `PowerUserAccess` role is insufficient - you'll need administrative privileges.
 
 #### Deployment
 
